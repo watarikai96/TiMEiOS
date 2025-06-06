a77c9d61f5cadad1520b3d48615184021e737a31
rahul2025 <rs3@wgu.edu>
2025-06-06 16:24:27 +0900

    Initial Commit

---
+++ TiME/TiMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiMEApp.swift
+//  TiME
+//
+//  Created by 渡海 on 2025/06/06.
+//
+
+import SwiftUI
+
+@main
+struct TiMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
